3db5e773cedb457abfa7a89db4935463e9aafdb1
2358221 <tshuto.1014@gmail.com>
2024-07-29 00:39:06 +0900

    Delete JS directory

--- JS/page2.js
+++
@@ -1,8 +0,0 @@
-function chk(quizName, correctAnswerId) {
-    const selectedAnswer = document.querySelector(`input[name="${quizName}"]:checked`);
-    if (selectedAnswer && selectedAnswer.id === correctAnswerId) {
-        alert('正解です！');
-    } else {
-        alert('不正解です。もう一度試してください。');
-    }
-}
